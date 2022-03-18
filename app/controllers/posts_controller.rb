215366b9b46ee605f914283ccc66f379df7cd850
Adam Harbisher <aharbisher@gmail.com>
2022-03-18 19:04:11 +0000

    Users can now edit their own posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,27 @@ class PostsController < ApplicationController
     redirect_to topic_path(@topic)
   end
 
+  def edit
+    if @user.id == Post.find(params[:id]).user.id
+      @post = Post.find(params[:id])
+    else
+      redirect_to topic_path(Post.find(params[:id]).topic)
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @user.id == @post.user.id
+      if @post.update(post_params)
+        redirect_to topic_path(Post.find(params[:id]).topic)
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    else
+      redirect_to topic_path(Post.find(params[:id]).topic)
+    end
+  end 
+
   private
     def post_params
       params.require(:post).permit(:body)
