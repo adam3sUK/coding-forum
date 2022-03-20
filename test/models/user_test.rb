ed7d31d66061b1f0fa18b91332df748ce588bc0e
Adam Harbisher <aharbisher@gmail.com>
2022-03-20 18:18:50 +0000

    used time_ago_in_words method to udpate some times for readability

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+  test "shoud not save user without params" do
+    user = User.new
+    assert_not user.save
+  end
   # test "the truth" do
   #   assert true
   # end
